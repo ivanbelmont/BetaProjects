bd56134fbc7476c118f3022bf3368a2f2fa97c43
ivanbelmont <potara.cotosta@gmail.com>
2016-03-25 00:26:34 -0600

    Cambios

--- conexion.php
+++ conexion.php
@@ -4,8 +4,8 @@ function conectar($con)
 {
     if ($con == 1) {
         # Conectar...
-        $mysqli = new mysqli('localhost','root','','comidas');//Local 
-        #$mysqli = new mysqli("mysql.hostinger.es", "u824935582_admin", "mgWwqJ73cLa", "u824935582_beta");//HOSTING
+        #$mysqli = new mysqli('localhost','root','','comidas');//Local 
+        $mysqli = new mysqli("mysql.hostinger.es", "u824935582_admin", "mgWwqJ73cLa", "u824935582_beta");//HOSTING
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
@@ -14,8 +14,8 @@ if ($mysqli->connect_errno) {
     if ($con == 0) {
 
         # Desconectar...
-       $mysqli = new mysqli('localhost','root','','comidas');//Local 
-       #$mysqli = new mysqli("mysql.hostinger.es", "u824935582_admin", "mgWwqJ73cLa", "u824935582_beta");
+       #$mysqli = new mysqli('localhost','root','','comidas');//Local 
+       $mysqli = new mysqli("mysql.hostinger.es", "u824935582_admin", "mgWwqJ73cLa", "u824935582_beta");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         $mysqli->close();
